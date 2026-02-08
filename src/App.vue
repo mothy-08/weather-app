4e7aec2981941dd704345e6c89e5314a8d872997
mothy-08 <119960367+mothy-08@users.noreply.github.com>
2026-02-08 21:38:03 +0800

    feat: created header and searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,32 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header class="flex justify-between px-16 py-8">
+    <div>
+      <img src="./assets/images/logo.svg" alt="Weather app logo" />
+    </div>
+    <!-- dropdown -->
+    <div>
+      <!-- settings icon -->
+      <span>Units</span>
+      <!-- arrow down icon  -->
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main class="flex flex-col items-center justify-center gap-8">
+    <h1 class="font-grotesque text-5xl">How's the sky looking today?</h1>
+
+    <div class="flex items-center justify-center gap-4 text-base">
+      <div class="flex w-96 items-center gap-3 rounded-lg bg-neutral-800 px-5 py-2">
+        <img src="./assets/images/icon-search.svg" alt="Search icon" />
+        <input
+          type="text"
+          class="placeholder:font-medium placeholder:text-neutral-200"
+          placeholder="Search for a place..."
+        />
+      </div>
+
+      <button class="rounded-lg bg-blue-500 px-5 py-2 font-medium">Search</button>
+    </div>
   </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/components/StatusCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  statusType: string
+  statusValue: string
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col items-center">
+    <span>{{ statusType }}</span>
+    <span>{{ statusValue }}</span>
+  </div>
+</template>
